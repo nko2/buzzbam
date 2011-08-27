66fe88adb73a1aa5a21fb603761debc6320d31a6
David Wolfe <david.p.wolfe@gmail.com>
2011-08-27 10:06:14 -0700

    log into facebook redirect link through ko

--- public/js/script.js
+++ public/js/script.js
@@ -19,6 +19,10 @@
 //  });
 //}
 
+function logIntoFacebook() {
+  window.location = 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login';
+}
+
 function updateUserData()
 {
   $.getJSON('friends', function(data) {
