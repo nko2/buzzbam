98f78364a9041a4db28c40c053877a9f07740a49
unknown <David@.(none)>
2011-08-26 21:33:39 -0700

    :updat to scripts and styles

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,18 @@
-/* Author:
+var couchInit = function (couch) {
+	
+}
 
-*/
+var createCouch = function (opt, callback) {
+	pouch.open(opt.name, function (err, db) {
+		ok(!err, 'created a pouch');
+		callback(db);
+	});
+};
+
+var viewModel = {
+  couch: createCouch({name: "partyplanner"}, couchInit),
+};
 
 
 
-
-
-
-
+ko.applyBindings(viewModel);
