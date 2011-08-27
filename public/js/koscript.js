e6371ef190a4b5b7ca02075aaa7f484b74ff3263
qbradley <qbradley@qbradleyd.qbradley.com>
2011-08-27 03:09:43 -0700

    Load friends in viewModel

--- public/js/koscript.js
+++ public/js/koscript.js
@@ -1,54 +1,56 @@
 var parties = [];
+var friends = [];
 
 var partyInfo = function (opt) {
-	this.id = opt.id;
-	this.isPublic = opt.isPublic;
-	this.title = opt.title;
-	this.description = opt.description;
-	this.users = ko.observableArray([new user(opt.creator)]);
-	this.items = ko.observableArray([]);
-	this.whereInfo = new whereInfo(opt.whereInfo);
-	this.whenInfo = new whenInfo(opt.whenInfo);
+  this.id = opt.id;
+  this.isPublic = opt.isPublic;
+  this.title = opt.title;
+  this.description = opt.description;
+  this.users = ko.observableArray([new user(opt.creator)]);
+  this.items = ko.observableArray([]);
+  this.whereInfo = new whereInfo(opt.whereInfo);
+  this.whenInfo = new whenInfo(opt.whenInfo);
 };
 
 var whereInfo = function (opt) {
-	this.id = opt.id;
-	this.location = opt.location;
+  this.id = opt.id;
+  this.location = opt.location;
 };
 
 var whenInfo = function (opt) {
-	this.id = opt.id;
-	this.startTime = opt.startTime;
-	this.endTime = opt.endTime;
+  this.id = opt.id;
+  this.startTime = opt.startTime;
+  this.endTime = opt.endTime;
 };
 
 var user = function (opt) {
-	this.id = opt.id;
-	this.userId = opt.userId;
-	this.fullName = opt.fullName;
-	this.email = opt.email;
-	this.role = opt.role ? opt.role : 'contrib';
-	this.rsvp = opt.rsvp ? opt.rsvp : 'maybe';
+  this.id = opt.id;
+  this.userId = opt.userId;
+  this.fullName = opt.fullName;
+  this.email = opt.email;
+  this.role = opt.role ? opt.role : 'contrib';
+  this.rsvp = opt.rsvp ? opt.rsvp : 'maybe';
 };
 
 var item = function (opt) {
-	this.id = opt.id;
-	this.isTask = opt.isTask;
-	this.isDone = opt.isDone;
-	this.description = opt.description;
-	this.comments = ko.observableArray(opt.comments ? opt.comments : []);
+  this.id = opt.id;
+  this.isTask = opt.isTask;
+  this.isDone = opt.isDone;
+  this.description = opt.description;
+  this.comments = ko.observableArray(opt.comments ? opt.comments : []);
 };
 
 var comment = function (opt) {
-	this.id = opt.id;
-	this.userId = opt.userId;
-	this.text = opt.text;
-	this.time = opt.time ? opt.time : Date.now();
+  this.id = opt.id;
+  this.userId = opt.userId;
+  this.text = opt.text;
+  this.time = opt.time ? opt.time : Date.now();
 }
-	
+  
 var viewModel = {
-	userFullName: ko.observable();
-	parties: ko.observableArray(parties);
+  userFullName: ko.observable(),
+  friends: ko.observableArray(friends),
+  parties: ko.observableArray(parties)
 };
 
 ko.applyBindings(viewModel);
@@ -57,19 +59,20 @@ function populateParties()
 {
   $(document).ready(function() {
     $.getJSON('parties', function(data) {
-			parties.clear();
-			for (var i in data) {
-				parties.add(new partyInfo({
-				  id: data[i].id,
-					isPublic: data[i].public,
-					title: data[i].title,
-					description: data[i].description,
-					userIds: data[i].users,
-					itemIds: data[i].items,
-					whereId: data[i].whereId,
-					whenId: data[i].whenId,
-			  }));
-			}
+      var newParties = [];
+      for (var i in data) {
+        newParties.push(new partyInfo({
+          id: data[i].id,
+          isPublic: data[i].public,
+          title: data[i].title,
+          description: data[i].description,
+          userIds: data[i].users,
+          itemIds: data[i].items,
+          whereId: data[i].whereId,
+          whenId: data[i].whenId,
+        }));
+      }
+      viewModel.parties(newParties);
     });
   });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,34 @@
 function updateUserStatus()
 {
-  $(document).ready(function() {
-    $.getJSON('getuser', function(data) {
-      if (data.user) {
-        var anchor = $('<a/>');
-        anchor.click(function(e) {
-          //$.getJSON('logout', updateUserStatus);
-          window.location = 'http://www.facebook.com/logout.php';
-        });
-        anchor.text('Logout (' + data.user.name + ')');
-        $('.loggedin').empty().append(anchor);
-      }
-      else {
-        var anchor = $('<a/>');
-        anchor.attr('href', 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login');
-        anchor.text('Login with Facebook');
-        $('.loggedin').empty().append(anchor);
-      }
-    });
+  $.getJSON('getuser', function(data) {
+    if (data.user) {
+      var anchor = $('<a/>');
+      anchor.click(function(e) {
+	//$.getJSON('logout', updateUserStatus);
+	window.location = 'http://www.facebook.com/logout.php';
+      });
+      anchor.text('Logout (' + data.user.name + ')');
+      $('.loggedin').empty().append(anchor);
+    }
+    else {
+      var anchor = $('<a/>');
+      anchor.attr('href', 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login');
+      anchor.text('Login with Facebook');
+      $('.loggedin').empty().append(anchor);
+    }
+  });
+}
+
+function updateUserData()
+{
+  $.getJSON('friends', function(data) {
+    viewModel.userFullName(data.me.name);
+    viewModel.friends(data.me.friends);
   });
 }
 
 $(document).ready(function() {
   updateUserStatus();
+  updateUserData();
 });
 
