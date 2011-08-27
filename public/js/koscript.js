1d3de869f09a235b664e2fc33a4ef4f826e248a8
David Wolfe <david.p.wolfe@gmail.com>
2011-08-27 08:39:14 -0700

    Port changes from no.de repo

---
+++ public/js/koscript.js
@@ -0,0 +1,101 @@
+var parties = [];
+var friends = [];
+
+var partyInfo = function (opt) {
+  this.id = opt.id;
+  this.isPublic = opt.isPublic;
+  this.title = opt.title;
+  this.description = opt.description;
+  this.users = ko.observableArray();//[new user(opt.creator)]);
+  this.userIds = ko.observableArray(opt.userIds);
+  this.items = ko.observableArray([]);
+  this.whereInfo = new whereInfo(opt.whereInfo);o
+  this.whenInfo = new whenInfo(opt.whenInfo);
+};
+
+var whereInfo = function (opt) {
+  this.id = opt.id;
+  this.location = opt.location;
+};
+
+var whenInfo = function (opt) {
+  this.id = opt.id;
+  this.startTime = opt.startTime;
+  this.endTime = opt.endTime;
+};
+
+var user = function (opt) {
+  this.id = opt.id;
+  this.userId = opt.userId;
+  this.fullName = opt.fullName;
+  this.email = opt.email;
+  this.role = opt.role ? opt.role : 'contrib';
+  this.rsvp = opt.rsvp ? opt.rsvp : 'maybe';
+};
+
+var friend = function (opt) {
+	this.userId = opt.userId;
+	this.name = opt.name;
+	
+	this.remove = function () { viewModel.friends.remove(this); }
+}
+
+var item = function (opt) {
+  this.id = opt.id;
+  this.isTask = opt.isTask;
+  this.isDone = opt.isDone;
+  this.description = opt.description;
+  this.comments = ko.observableArray(opt.comments ? opt.comments : []);
+};
+
+var comment = function (opt) {
+  this.id = opt.id;
+  this.userId = opt.userId;
+  this.text = opt.text;
+  this.time = opt.time ? opt.time : Date.now();
+}
+  
+var viewModel = {
+  userFullName: ko.observable(),
+  isLoggedIn: ko.observable(false),
+  friends: ko.observableArray(friends),
+  parties: ko.observableArray(parties),
+};
+
+ko.applyBindings(viewModel);
+
+function populateParty(party)
+{
+  var userIds = party.userIds;
+  for (var i in party.userIds) {
+    $.getJSON('user?id=' + userIds[i], function(data) {
+      party.users.push(new user({ }));
+    });
+  }
+}
+
+function populateParties()
+{
+  $(document).ready(function() {
+    $.getJSON('parties', function(data) {
+      var newParties = [];
+      for (var i in data) {
+        newParties.push(new partyInfo({
+          id: data[i].id,
+          isPublic: data[i].public,
+          title: data[i].title,
+          description: data[i].description,
+          userIds: data[i].users,
+          itemIds: data[i].items,
+          whereId: data[i].whereId,
+          whenId: data[i].whenId,
+        }));
+      }
+      viewModel.parties(newParties);
+    });
+  });
+}
+
+$(document).ready(function() {
+  populateParties();
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,68 @@
 function updateUserStatus()
 {
-  $(document).ready(function() {
-    $.getJSON('getuser', function(data) {
-      if (data.user) {
-        var anchor = $('<a/>');
-        anchor.click(function(e) {
-          //$.getJSON('logout', updateUserStatus);
-          window.location = 'http://www.facebook.com/logout.php';
-        });
-        anchor.text('Logout (' + data.user.name + ')');
-        $('.loggedin').empty().append(anchor);
-      }
-      else {
-        var anchor = $('<a/>');
-        anchor.attr('href', 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login');
-        anchor.text('Login with Facebook');
-        $('.loggedin').empty().append(anchor);
-      }
-    });
+  $.getJSON('getuser', function(data) {
+    if (data.user) {
+      var anchor = $('<a/>');
+      anchor.click(function(e) {
+        //$.getJSON('logout', updateUserStatus);
+        window.location = 'http://www.facebook.com/logout.php';
+      });
+      anchor.text('Logout (' + data.user.name + ')');
+      $('.loggedin').empty().append(anchor);
+    }
+    else {
+      var anchor = $('<a/>');
+      anchor.attr('href', 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login');
+      anchor.text('Login with Facebook');
+      $('.loggedin').empty().append(anchor);
+    }
   });
 }
 
+function updateUserData()
+{
+  $.getJSON('friends', function(data) {
+    viewModel.userFullName(data.me.name);
+    viewModel.friends(data.me.friends);
+  });
+}
+
+function log(x) {
+  console.log(x);
+}
+
+var server = (function(){
+  var that = {};
+
+  that.getUser = function(id, callback) {
+    $.getJSON('user', {id:id}, callback);
+  };
+
+  that.getFriends = function(callback) {
+    $.getJSON('friends', callback);
+  };
+
+  that.newParty = function(title, description, callback) {
+    var data = { title: title };
+    if (description) {
+      data.description = description;
+    }
+    $.getJSON('newparty', data, callback);
+  };
+
+  that.getParty = function(id, callback) {
+    $.getJSON('party', {id:id}, callback);
+  };
+
+  that.getParties = function(callback) {
+    $.getJSON('parties', callback);
+  };
+
+  return that;
+})();
+
 $(document).ready(function() {
   updateUserStatus();
+  updateUserData();
 });
 
