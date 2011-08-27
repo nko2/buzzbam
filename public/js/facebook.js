a28c3289c6778d0836420e3c933eee6d2bf63197
David Wolfe <david.p.wolfe@gmail.com>
2011-08-27 14:03:32 -0700

    factoring out some code to separate js files

---
+++ public/js/facebook.js
@@ -0,0 +1,8 @@
+FB.init({
+  appId  : '225589484159909',
+  status : true, // check login status
+  cookie : true, // enable cookies to allow the server to access the session
+  xfbml  : true, // parse XFBML
+  channelUrl : 'http://partyplanner.no.de/channel.html', // channel.html file
+  oauth  : true // enable OAuth 2.0
+});
---
+++ public/js/query.tmpl.js
@@ -0,0 +1,7 @@
+<script type="text/x-jquery-tmpl" id="userTemplate">
+    <tr>
+        <td><span data-bind="text: name"></span></td>
+        <td><span data-bind="text: userId"></span></td>
+        <td><a href="#" data-bind="click: remove">Remove</a></td>
+    </tr>
+</script>
