7b692ac5acb084bfa4ecf66c2308b94a2126967f
qbradley <qbradley@qbradleyd.qbradley.com>
2011-08-26 22:36:00 -0700

    Delete pouch

--- public/js/script.js
+++ public/js/script.js
@@ -16,21 +16,8 @@ $(document).ready(function() {
   });
 });
 
-var couchInit = function (couch) {
-	
-}
-
-var createCouch = function (opt, callback) {
-	pouch.open(opt.name, function (err, db) {
-		ok(!err, 'created a pouch');
-		callback(db);
-	});
-};
-
 var viewModel = {
-  couch: createCouch({name: "partyplanner"}, couchInit),
 };
-
-
 
 ko.applyBindings(viewModel);
+
