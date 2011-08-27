ed54e6e9951ee51559734ba532256f5f9052fb2a
David Wolfe <david.p.wolfe@gmail.com>
2011-08-27 13:45:54 -0700

    Update viewModel user

--- public/js/viewmodel.js
+++ public/js/viewmodel.js
@@ -23,7 +23,7 @@ var user = function (opt) {
   this.role = opt.role ? opt.role : 'contrib';
   this.rsvp = opt.rsvp ? opt.rsvp : 'maybe';
   this.timezone = opt.timezone;
-  this.
+  this.localse: opt.locale;
 };
 
 var friend = function (opt) {
