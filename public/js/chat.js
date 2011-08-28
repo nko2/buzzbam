653658b7e833118ff21ff59141950011358c673c
Quetzal Bradley <qbradley@qbradley.com>
2011-08-28 00:05:34 -0700

    chatting progress

--- public/js/chat.js
+++ public/js/chat.js
@@ -23,6 +23,12 @@ function getChats()
 function prepareChat()
 {
   // get it started
+  server.getComments("59a7245936b94ea159abbbd75a001000", function (chats) {
+    for (var index in chats) {
+      var chatid = chats[index];
+      loadChat(chatid);
+    }
+  });
   getChats();
 }
 
