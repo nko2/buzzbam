293fd3b70283f4ad7c0a986750fd78a371e432c1
David Wolfe <david.p.wolfe@gmail.com>
2011-08-28 12:44:22 -0700

    totally changing how we use the view and viewmodel for the home.html

---
+++ public/js/homeModel.js
@@ -0,0 +1,100 @@
+// MODEL
+var model = {
+  parties: [];
+  user: undefined;
+  isLoggedIn: false;
+};
+
+function modelSetParties(parties) {
+  model.parties = parties;
+  viewModel.parties(parties);
+};
+
+function modelSetUser(user) {
+  model.user = user;
+  viewModel.user(user);
+};
+
+function modelSetIsLoggedIn(isLoggedIn) {
+  model.isLoggedIn = isLoggedIn;
+  viewModel.isLoggedIn(isLoggedIn);
+};
+
+// VIEWMODEL
+var viewModel = {
+  parties: ko.observableCollection([]),
+  user: ko.observable();
+  isLoggedIn: ko.observable(false);
+};
+
+viewModel.logInOutOfFacebook = function() {
+  if (!viewModel.isLoggedIn()) {
+    window.location = 'https://www.facebook.com/dialog/oauth?client_id=225589484159909&redirect_uri=http://partyplanner.no.de/login';
+  } else {
+    FB.logout(function(resp) {
+      viewModel.user(new user({}));
+      viewModel.isLoggedIn(false);
+    });
+  }
+};
+
+viewModel.createNewParty = function() {
+  // get information from form
+  var title = "default title";
+  var description = "default description";
+  server.newParty(title, description, function(data) {
+      var newParty = parseParty(data);
+      window.location = 'index.html?partyId=' + newParty.id;
+    });
+};
+
+viewModel.getPublicParties = ko.dependentObservable(function() {
+  var parties = viewModel.parties();
+  var publicParties = [];
+  for (var i in parties) {
+    if (parties[i].isPublic) {
+      publicParties.push(parties[i]);
+    }
+  };
+  return publicParties;
+}
+
+viewModel.getPrivateParties = ko.dependentObservable(function() {
+  var parties = viewModel.parties();
+  var privateParties = [];
+  for (var i in parties) {
+    if (!parties[i].isPublic) {
+      privateParties.push(parties[i]);
+    }
+  };
+  return privateParties;
+}
+
+viewModel.redirectToParty = function(partyId) {
+  window.location = 'http://partyplanner.no.de/index.html?partyId=' + partyId;
+};
+
+// FUNCTIONS
+function loadData() {
+  if (window.location.hash === '#debug') {
+    server = server_local;
+  }
+  server.getUserAndFriends(function(data) {
+      modelSetUser(data.me);
+      model.isLoggedIn(data.me.id ? true : false);
+    });
+  server.getParties(function(parties) {
+      modelSetParties(parties);
+    });
+};
+
+
+// READY
+$(document).ready(function() {
+  if (window.location.hash === '#debug') {
+    server = server_local;
+  }
+  
+  ko.applyBindings(viewModel);
+  loadData();
+});
